cc0ab99966588b81dc696b20ead9cce5f8b208e9
Sam Squire <sam@samsquire.com>
2019-11-11 21:18:05 +0000

    Sam pull out data into separate file

--- layout/layouter.py
+++ layout/layouter.py
@@ -2,140 +2,16 @@ import collections
 from ortools.sat.python import cp_model
 from pprint import pprint
 import itertools
+import json
+import sys
 
-widgets = {
-    "blogHeading": {
-        "html": "<h1>Sample blog post</h1>"
-    },
-    "postMetadata": {
-        "html": "<p class=\"blog-post-meta\">January 1, 2014 by <a href=\"author\">Mark</a></p>"
-    },
-    "posting": {
-        "html": """<p>This blog post shows a few different types of content that's supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
-            <hr>
-            <p>Cum sociis natoque penatibus et magnis <a href=\"#\">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
-            <blockquote>
-              <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-            </blockquote>
-            <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-            <h2>Heading</h2>
-            <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-            <h3>Sub-heading</h3>
-            <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-            <pre><code>Example code block</code></pre>
-            <p>Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
-            <h3>Sub-heading</h3>
-            <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-            <ul>"""
-    },
-    "blogPost": {
-        "predicates": [
-            "blogHeading above postMetadata",
-            "posting under postMetadata"
-        ]
-    },
-    "introduction": { "html": "<h3 class=\"pb-3 mb-4 font-italic border-bottom\">From the firehose</h3>", "classes": "font-italic" },
-    "blogs": {
-        "predicates": [
-            "introduction above blogPost"
-        ]
-    },
-    "logo": {
-        "html": "<h1 class=\"blog-header-logo\">Large</h1>"
-    },
-    "header": {
-        "predicates": [
-            "logo hasSize 12"
-        ],
-        "classes": "blog-header py-3 justify-content-between align-items-center text-center"
-    },
-    "featuredPosts": {
-        "predicates": [
-            "featuredPostA hasSize 6",
-            "featuredPostB hasSize 6",
-            "featuredPostB rightOf featuredPostA",
-        ]
-    },
-    "menu": {
-        "predicates": [
-            "U.S. rightOf World",
-            "Technology rightOf U.S.",
-            "Design rightOf Technology",
-            "Culture rightOf Design",
-            "Business rightOf Culture",
-            "Politics rightOf Business",
-            "Opinion rightOf Politics",
-            "Science rightOf Opinion",
-            "Health rightOf Science",
-            "Style rightOf Health",
-            "Travel rightOf Style"
-        ]
-    },
-    "heroPost": {
-        "predicates": [
-            "heroText above continueReadingLink"
-        ]
-    },
-    "heroText": {
-        "html": "<h2>Title of a longer featured blog post</h2>"
-    },
-    "continueReadingLink": {
-        "html": "<a href=\"featured-post-a\">Continue reading</a>"
-    },
-    "featuredPostA": {
-        "predicates": [
-            "featureTextA above continueReadingLink"
-        ]
-    },
-    "featuredPostB": {
-        "predicates": [
-            "featureTextB above continueReadingLink"
-        ]
-    },
-    "featureTextA": {
-        "html": "<h3>Some interesting article 1</h3>"
-    },
-    "featureTextB": {
-        "html": "<h3>Some interesting article 2</h3>"
-    },
-    "blogSidebar": {
-        "predicates": [
-            "aboutSection centered screen", 
-        ],  "classes": "bg-light"
-    },
-    "aboutSection": {
-        "predicates": [
-        "aboutTitle above aboutText",
-        "aboutText hasSize 12"
-        ]
-    },
-    "aboutText": {
-        "html": "Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur."
-    },
-    "aboutTitle": {
-        "html": "<h4 class=\"font-italic\">About</h4>"
-    }
-}
+filename = sys.argv[1]
+data = json.loads(open(filename).read())
+widgets = data["widgets"]
 
 # <link href="https://getbootstrap.com/docs/4.0/examples/blog/blog.css" rel="stylesheet">
 
-all_predicates = [
-    [
-        "header hasSize 12",
-        "header centered screen",
-        "header above menu",
-        "heroPost above featuredPosts",
-        "menu centered screen",
-        "menu hasSize 12",
-        "heroPost under menu",
-        "heroPost hasSize 12",
-        "menu above heroPost"
-    ],
-    [
-        "blogs hasSize 8",
-        "blogSidebar hasSize 4"
-    ]
-]
+all_predicates = data["predicates"]
 
 def layout_page(predicates, classes):
     """Schedule elements on a page"""
